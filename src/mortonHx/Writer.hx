8b703a0bd727befa4819d97e79643ccf05f70c54
nanjizal <nanjizal@users.noreply.github.com>
2026-01-03 23:16:32 +0000

    Implement Morton code generation from float coordinates
    Added methods for creating Morton codes from float coordinates with offsets.

--- src/mortonHx/Writer.hx
+++ src/mortonHx/Writer.hx
@@ -5,9 +5,21 @@ class Writer {
         return xy;
     }
     var xy: Int = 0;
+    final scaler = 32767;
     /**
      * Interleaves bits of two 16-bit integers into one 32-bit Morton code.
      */
+    public inline static function fromFloatXY( x: Float, y: Float, wid: Float, hi: Float ): Int {
+        var scaleX = scalar/wid;
+        var scaleY = scalar/hi;
+        return new Writer( Std.int( x * scaleX ), std.int( y * scaleY ) );
+    }
+    public inline static function fromFloatXYoffSet( x: Float, y: Float, xOff: Float, yOff: Float, wid: Float, hi: Float ): Int {
+        return new fromFloatXY( x - offX, y - offY );
+    }
+    public inline static function fromFloatX_YoffSet( x: Float, y: Float, xOff: Float, yOff: Float, wid: Float, hi: Float ): Int {
+        return new fromFloatXY( x - offX, -(y - offY) );
+    }
     public inline function new( x: Int, y: Int ): Int {
         xy = ( part1By1( y ) << 1 ) | part1By1( x );
     }
