e1d2a9e8eadac745ae741c0166d938d48942b9ea
nanjizal <nanjizal@users.noreply.github.com>
2026-01-03 23:00:57 +0000

    Change functions to inline in Reader.hx

--- src/mortonHx/Reader.hx
+++ src/mortonHx/Reader.hx
@@ -2,17 +2,17 @@ import mortonHx.Writer;
 public class Reader {
   var x: Int = 0; 
   var y: Int = 0;
-  public static function fromWriter( v: Writer ): Reader {
+  public static inline function fromWriter( v: Writer ): Reader {
     return fromWriterXY( morton.x, morton.y );
   }
-  public static function fromWriterXY( morton: Int ): Reader {
+  public static inline function fromWriterXY( morton: Int ): Reader {
     return new Reader( x, y );
   }
-  public function new( x: Int, y: Int ){
+  public inline function new( x: Int, y: Int ){
     x = compact1By1( morton );
     y = compact1By1( morton >> 1 );
   }
-  function compact1By1(j:Int):Int {
+  function inline compact1By1(j:Int):Int {
     j &= 0x55555555;
     j = (j ^ (j >> 1)) & 0x33333333;
     j = (j ^ (j >> 2)) & 0x0f0f0f0f;
