c045c780eda52242c3f9c3db7c2386b5687ea5ea
nanjizal <nanjizal@users.noreply.github.com>
2026-01-04 00:21:02 +0000

    Implement IntScale abstract with scaling functions
    Add IntScale abstract with constructor and toFloatXY function.

--- src/mortonHx/IntScale.hx
+++ src/mortonHx/IntScale.hx
@@ -1,1 +1,23 @@
+package mortonHx;
+import mortonHx.Reader;
+import mortonHx.Writer;
 
+abstract IntScale from Int to Int {
+    public inline function new( v: Int ){
+        this = v;
+    }
+    final scaler = 32767;
+    /*public inline static function fromFloatXY( x: Float, y: Float, wid: Float, hi: Float ): Writer {
+        var scaleX = scalar/wid;
+        var scaleY = scalar/hi;
+        return new Writer( Std.int( x * scaleX ), Std.int( y * scaleY ) );
+    }
+*/
+    public inline static function toFloatXY( v: j, wid: Float, hi: Float ): Int {
+        var out = new Reader( v );
+        
+        var scaleX = scalar/wid;
+        var scaleY = scalar/hi;
+        return new Reader( Std.int( x * scaleX ), Std.int( y * scaleY ) );
+    }
+}
