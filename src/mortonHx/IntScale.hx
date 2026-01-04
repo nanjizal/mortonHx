89bd7348793ba0b6dfc21daa81c5b49f6a79e1d5
nanjizal <nanjizal@users.noreply.github.com>
2026-01-04 00:26:27 +0000

    Refactor toFloatXY for improved scaling calculation

--- src/mortonHx/IntScale.hx
+++ src/mortonHx/IntScale.hx
@@ -15,9 +15,8 @@ abstract IntScale from Int to Int {
 */
     public inline static function toFloatXY( v: j, wid: Float, hi: Float ): Int {
         var out = new Reader( v );
-        
-        var scaleX = scalar/wid;
-        var scaleY = scalar/hi;
-        return new Reader( Std.int( x * scaleX ), Std.int( y * scaleY ) );
+        var scaleX = wid*scaler;
+        var scaleY = hi*scalar;
+        return { x: out.x * scaleX , y: out.y * scaleY };
     }
 }
